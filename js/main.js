b39cfe53bd02fc0cbbed9fc599c38ba7ac86b50f
Fergster1299 <fergdyl@gmail.com>
2025-07-13 15:16:25 -0400

    added search page and sorted cards

--- js/main.js
+++ js/main.js
@@ -1,18 +1,22 @@
+apiKey = 'a944a4e4-f699-4628-9083-8f831562154a'
 
 function search(){
-fetch('https://api.pokemontcg.io/v2/cards')
+fetch(`https://api.pokemontcg.io/v2/cards`)
 .then(res => res.json()) // parse response as JSON
 .then(data => {
   console.log(data)
-
     data.data.forEach(num => {
-        let imgElement = document.createElement('img');
         let pokemon = num.images.small;
-        imgElement.src = pokemon
-        document.body.appendChild(imgElement);
+
+        const myImage = document.createElement('img')
+        myImage.src = pokemon
+        const cardSection = document.getElementById('cards')
+        cardSection.appendChild(myImage);
     })
 })
 .catch(err => {
     console.log(`error ${err}`)
 });
-}
+};
+
+search()
